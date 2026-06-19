f9238feb2ae51271a4cde8508fe9728fb1192391
sarajinak <sarakhadka324@gmail.com>
2026-06-19 19:22:04 +0545

    Feat: Theme Colours

--- app/src/main/java/com/example/studygether/ui/theme/Color.kt
+++ app/src/main/java/com/example/studygether/ui/theme/Color.kt
@@ -9,3 +9,36 @@ val CardColour = Color(0xFFBBB8B8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val LightBlueP = Color(0xFF317EB2)
+val LightBlueS = Color(0xFF587398)
+val LightBlueT = Color(0xFF6D5BAF)
+
+val DarkBlueP = Color(0xFF0B2C54)
+val DarkBlueS = Color(0xFF2F4162)
+val DarkBlueT = Color(0xFF4F477C)
+
+val LightYellowP = Color(0xFFE3CA7C)
+val LightYellowS= Color(0xFFE1A0D3)
+val LightYellowT = Color(0xFFE5A385)
+
+val DarkYellowP = Color(0xFFDAC34D)
+val DarkYellowS = Color(0xFF734C6E)
+val DarkYellowT = Color(0xFFB07E66)
+
+val LightGreenP = Color(0xFF87E38B)
+val LightGreenS = Color(0xFF86D08D)
+val LightGreenT = Color(0xFFA3E1BC)
+
+val DarkGreenP = Color(0xFF3F6B40)
+val DarkGreenS = Color(0xFF446447)
+val DarkGreenT = Color(0xFF53725F)
+
+val LightOrangeP = Color(0xFFCE8254)
+val LightOrangeS = Color(0xFFCB9687)
+val LightOrangeT = Color(0xFFBB7FA0)
+
+val DarkOrangeP = Color(0xFFA25D42)
+val DarkOrangeS = Color(0xFF814B4B)
+val DarkOrangeT = Color(0xFF694559)
